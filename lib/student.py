70841a86059fbe266b657002b39d85f2301d51b3
mitch kamau <michchore@gmail.com>
2025-02-26 15:11:51 +0300

    main

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,15 @@
-#!/usr/bin/env python
-
 from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    # Setting up the student's first and last name, and an empty list for knowledge
+    def __init__(self, first_name, last_name):
+        # Using the parent class to set the name
+        super().__init__(first_name, last_name)
+        
+        # Creating an empty list to store what the student learns
+        self.knowledge = []
+
+    # Method for the student to learn something new
+    def learn(self, knowledge_string):
+        self.knowledge.append(knowledge_string)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python
-
 from user import User
 
 import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "Learn how to use python"
+            "learn inheritance bit of it"
+
+        ]
+
     def teach(self):
-        pass
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
